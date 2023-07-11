2247a350b91a4e3442e40213abc3af61e30b3e00
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-07-11 13:33:58 +0300

    routes, redirect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque assumenda molestiae ut nisi totam maiores quam delectus praesentium, expedita soluta aspernatur saepe eveniet minus quis consectetur dolorem minima quae beatae?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque assumenda molestiae ut nisi totam maiores quam delectus praesentium, expedita soluta aspernatur saepe eveniet minus quis consectetur dolorem minima quae beatae?</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
